8dfa372a84a3a3208c00a3eb3d8c166972931d5e
Stanislav Gatev <sg@sgatev.com>
2022-08-07 17:23:58 +0200

    Add more comments and make them consistent

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ DOTFILES_DIR=$(cd `dirname $0` && pwd)
 # https://brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# tmux
+# http://tmux.github.io
 brew install --quiet tmux
 ln -f -s $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 brew install --quiet reattach-to-user-namespace
 
-# zsh
+# https://zsh.org
 brew install --quiet zsh
 brew install --quiet zsh-autosuggestions
 
@@ -22,13 +22,13 @@ mkdir -p ~/.config && ln -f -s $DOTFILES_DIR/starship.toml ~/.config/starship.to
 brew tap homebrew/cask-fonts
 brew install --cask --quiet font-fira-code-nerd-font
 
-# vim
+# https://neovim.io
 brew install --quiet neovim
 sh -c 'curl -sfLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config && ln -f -s $DOTFILES_DIR/nvim ~/.config/
 
-# bazel
+# https://bazel.build
 brew install bazelisk
 brew tap bazelbuild/tap
 brew install bazelbuild/tap/ibazel
--- zshrc
+++ zshrc
@@ -1,14 +1,19 @@
+# ensure that a tmux session is attached
 if [ -z "$TMUX" ]
 then
-    tmux attach -t TMUX || tmux new -s TMUX
+  tmux attach -t TMUX || tmux new -s TMUX
 fi
 
+# set up zsh plugins
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# initialize https://starship.rs
+eval "$(starship init zsh)"
+
+# configure history
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_ALL_DUPS
 
-eval "$(starship init zsh)"
-
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
+# set up aliases
 alias e=nvim
 alias l="ls -alG"
