9461e10d115f82eedcdfafef51942aeffe720175
Stanislav Gatev <sg@sgatev.com>
2022-08-28 19:10:26 +0200

    Split neovim config in multiple files

---
+++ nvim/lua/custom/telescope.lua
@@ -0,0 +1,27 @@
+local actions = require("telescope.actions")
+
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        -- Do not to enter a normal-like mode when hitting escape.
+        ["<esc>"] = actions.close
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      -- Remove ./ from fd results.
+      find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true
+    }
+  }
+}
+
+require('telescope').load_extension('fzf')
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = ","
+vim.g.lightline = {
+  colorscheme = "nord"
+}
+
+vim.api.nvim_command([[colorscheme nord]])
+
+vim.o.termguicolors = true -- emit true (24-bit) colors
+vim.o.showmode = false     -- do not show the current mode
+vim.o.number = true        -- show line numbers on the left
+vim.o.cursorline = true    -- highlight the current line
+vim.o.tabstop = 2          -- 1 tab = 2 spaces
+vim.o.softtabstop = 2      -- show existing tabs as 2 spaces
+vim.o.shiftwidth = 2       -- indent with 2 spaces
+vim.o.expandtab = true     -- expand tab to spaces
