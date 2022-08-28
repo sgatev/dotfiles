a718bb2925f2ef01978653b9a3c5a86bd9bb9007
Stanislav Gatev <sg@sgatev.com>
2022-08-29 00:06:19 +0200

    Enable tmux-neovim clipboard interop

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -4,11 +4,12 @@ vim.g.lightline = {
   colorscheme = 'nord'
 }
 
-vim.o.termguicolors = true -- emit true (24-bit) colors
-vim.o.showmode = false     -- do not show the current mode
-vim.o.number = true        -- show line numbers on the left
-vim.o.cursorline = true    -- highlight the current line
-vim.o.tabstop = 2          -- 1 tab = 2 spaces
-vim.o.softtabstop = 2      -- show existing tabs as 2 spaces
-vim.o.shiftwidth = 2       -- indent with 2 spaces
-vim.o.expandtab = true     -- expand tab to spaces
+vim.o.termguicolors = true  -- emit true (24-bit) colors
+vim.o.showmode = false      -- do not show the current mode
+vim.o.number = true         -- show line numbers on the left
+vim.o.cursorline = true     -- highlight the current line
+vim.o.tabstop = 2           -- 1 tab = 2 spaces
+vim.o.softtabstop = 2       -- show existing tabs as 2 spaces
+vim.o.shiftwidth = 2        -- indent with 2 spaces
+vim.o.expandtab = true      -- expand tab to spaces
+vim.o.clipboard = 'unnamed' -- copy/paste to system clipboard
