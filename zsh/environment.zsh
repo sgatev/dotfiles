015a1d91969d05317a9f17e9b4e9541e9c2a0cd7
Stanislav Gatev <sg@sgatev.com>
2025-01-30 22:08:04 +0100

    Add Obsidian notes to directory hash table

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,3 +1,5 @@
 export EDITOR="nvim"
 export PATH="$PATH:$HOME/bin"
 export PROMPT='%F{blue}%~ %(?.%F{green}❯.%F{red}❯)%f '
+
+hash -d notes=~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Vault
