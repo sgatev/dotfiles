5749de51622872380c5c9493a1329ff49720d5e4
Stanislav Gatev <sg@sgatev.com>
2025-04-27 15:17:34 +0200

    Configure kitty to jump over words

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,5 +1,6 @@
 export EDITOR="nvim"
 export PATH="$PATH:$HOME/bin"
 export PROMPT='%F{blue}%~ %(?.%F{green}❯.%F{red}❯)%f '
+export WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
 
 hash -d notes=~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Vault
