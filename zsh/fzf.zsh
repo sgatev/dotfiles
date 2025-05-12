45bc36dec8a747c0469f445b1eb9678364aae4a3
Stanislav Gatev <sg@sgatev.com>
2025-05-12 14:19:31 +0200

    Update fzf default style

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,6 +1,7 @@
-FZF_DEFAULT_COLOR="bg+:-1,pointer:#88C0D0,marker:#A3BE8C,info:#4C566A,spinner:#4C566A"
+FZF_DEFAULT_UI="--prompt='❯ ' --pointer='▶'"
+FZF_DEFAULT_COLOR="bg+:-1,prompt:#81A1C1,pointer:#81A1C1,marker:#A3BE8C,info:#4C566A,spinner:#4C566A,border:#4C566A"
 FZF_DEFAULT_PREVIEW="bat --theme=Nord --style=numbers --color=always --line-range :500 {}"
-export FZF_DEFAULT_OPTS="--color='$FZF_DEFAULT_COLOR' --preview '$FZF_DEFAULT_PREVIEW'"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_UI --color='$FZF_DEFAULT_COLOR' --preview='$FZF_DEFAULT_PREVIEW'"
 
 # https://github.com/Aloxaf/fzf-tab
 . ~/.zsh/plugins/fzf-tab/fzf-tab.plugin.zsh
