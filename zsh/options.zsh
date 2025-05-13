ab92637c3b5c96870613309972d67492a6591777
Stanislav Gatev <sg@sgatev.com>
2025-05-13 23:33:57 +0200

    Configure zsh history and autocd

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,5 +1,11 @@
-setopt APPEND_HISTORY       # Append history list to the history file.
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt APPEND_HISTORY       # Append to the history file.
+setopt INC_APPEND_HISTORY   # Write to the history file immediately, not when the shell exits.
 setopt EXTENDED_HISTORY     # Save command’s beginning timestamp and duration.
 setopt HIST_FIND_NO_DUPS    # Do not display duplicates.
 setopt HIST_IGNORE_ALL_DUPS # Remove older duplicate from history.
 setopt SHARE_HISTORY        # Share history between sessions.
+setopt AUTOCD               # Move to directories without cd.
