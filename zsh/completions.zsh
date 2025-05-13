ccb5506db58d789f1843ecc1914fb9ba68a7f73e
Stanislav Gatev <sg@sgatev.com>
2025-05-13 22:54:46 +0200

    Configure zsh completion

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,3 +1,7 @@
 fpath=("/opt/homebrew/share/zsh/site-functions" $fpath)
 
 autoload -Uz compinit && compinit
+
+zstyle ':completion:*' completer _expand_alias _extensions _complete _approximate
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
