985b1bea686d45c37fe8f35250b43e5446692cbf
Stanislav Gatev <sg@sgatev.com>
2025-05-16 19:48:14 +0200

    Use nvim for man pager

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,5 +1,5 @@
 export EDITOR="nvim"
-export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=ansi'"
+export MANPAGER="nvim +Man!"
 export PATH="$PATH:$HOME/bin"
 export PROMPT=$'%F{blue}%~\n%(?.%F{green}❯.%F{red}❯)%f '
 export WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
