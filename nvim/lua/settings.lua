7b6b826cf99a9bdb4c73c011397e31e89e4be978
Stanislav Gatev <sg@sgatev.com>
2025-05-17 11:28:55 +0200

    Show relative line numbers in nvim

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,13 +1,14 @@
 vim.g.mapleader = ','
 
-vim.o.termguicolors = true  -- emit true (24-bit) colors
-vim.o.showmode = false      -- do not show the current mode
-vim.o.number = true         -- show line numbers on the left
-vim.o.cursorline = true     -- highlight the current line
-vim.o.tabstop = 2           -- 1 tab = 2 spaces
-vim.o.softtabstop = 2       -- show existing tabs as 2 spaces
-vim.o.shiftwidth = 2        -- indent with 2 spaces
-vim.o.expandtab = true      -- expand tab to spaces
 vim.o.clipboard = 'unnamed' -- copy/paste to system clipboard
-vim.o.hlsearch = false      -- do not highlight search matches
 vim.o.conceallevel = 2      -- hide concealed text
+vim.o.cursorline = true     -- highlight the current line
+vim.o.expandtab = true      -- expand tab to spaces
+vim.o.hlsearch = false      -- do not highlight search matches
+vim.o.number = true         -- show line numbers on the left
+vim.o.relativenumber = true -- show relative line numbers
+vim.o.shiftwidth = 2        -- indent with 2 spaces
+vim.o.showmode = false      -- do not show the current mode
+vim.o.softtabstop = 2       -- show existing tabs as 2 spaces
+vim.o.tabstop = 2           -- 1 tab = 2 spaces
+vim.o.termguicolors = true  -- emit true (24-bit) colors
