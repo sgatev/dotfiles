b0ef134de20acd4d875cecc542e78db0db27b8ae
Stanislav Gatev <sg@sgatev.com>
2025-05-25 01:06:20 +0200

    Simplify lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -2,12 +2,20 @@ local lualine = require('lualine')
 local noice = require("noice")
 
 lualine.setup({
+  options = {
+    component_separators = '',
+    section_separators   = { left = '', right = '' },
+  },
   sections = {
+    lualine_b = { 'filename' },
+    lualine_c = {},
     lualine_x = {
       {
         noice.api.statusline.mode.get,
         cond = noice.api.statusline.mode.has,
-      }
+      },
     },
+    lualine_y = { 'filetype' },
+    lualine_z = {},
   },
 })
