51c1c9d1e290e3e37fd53cf64eb05c861f1dfa0e
Stanislav Gatev <sg@sgatev.com>
2025-05-25 13:12:15 +0200

    Add rename symbol keymap

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -16,3 +16,4 @@ lspconfig.lua_ls.setup({})
 lspconfig.bashls.setup({})
 
 vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Format buffer' })
+vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename symbol' })
