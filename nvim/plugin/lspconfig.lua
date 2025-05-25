4457464a1a03347adf2d53ea41a102a3a5d20e0d
Stanislav Gatev <sg@sgatev.com>
2025-05-25 17:40:57 +0200

    Add more code keymaps

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -1,5 +1,6 @@
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 local lspconfig = require('lspconfig')
+local fzf_lua = require('fzf-lua')
 
 vim.lsp.set_log_level("off")
 
@@ -15,5 +16,12 @@ lspconfig.lua_ls.setup({})
 
 lspconfig.bashls.setup({})
 
+vim.keymap.set('n', '<leader>cD', vim.lsp.buf.definition, { desc = 'Jump to definition' })
+vim.keymap.set('n', '<leader>ca', fzf_lua.lsp_code_actions, { desc = 'Open code actions' })
+vim.keymap.set('n', '<leader>cc', fzf_lua.lsp_incoming_calls, { desc = 'Open incoming calls' })
+vim.keymap.set('n', '<leader>cd', vim.lsp.buf.declaration, { desc = 'Jump to declaration' })
+vim.keymap.set('n', '<leader>ce', fzf_lua.diagnostics_document, { desc = 'Open diagnostics' })
 vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Format buffer' })
 vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+vim.keymap.set('n', '<leader>cs', fzf_lua.lsp_document_symbols, { desc = 'Open symbols' })
+vim.keymap.set('n', '<leader>cx', fzf_lua.lsp_references, { desc = 'Open references' })
