20ede1e4f8fba3e788f83fa5527d85349ae1b502
Stanislav Gatev <sg@sgatev.com>
2026-05-14 11:23:52 +0200

    Increase the default timeout of format

--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -22,12 +22,19 @@ vim.lsp.config('lua_ls', {})
 
 vim.lsp.enable('bashls')
 
+-- Formats a buffer.
+local function format()
+  return vim.lsp.buf.format({
+    timeout_ms = 10000,
+  })
+end
+
 vim.keymap.set('n', '<leader>cD', vim.lsp.buf.definition, { desc = 'Jump to definition' })
 vim.keymap.set('n', '<leader>ca', fzf_lua.lsp_code_actions, { desc = 'Open code actions' })
 vim.keymap.set('n', '<leader>cc', fzf_lua.lsp_incoming_calls, { desc = 'Open incoming calls' })
 vim.keymap.set('n', '<leader>cd', vim.lsp.buf.declaration, { desc = 'Jump to declaration' })
 vim.keymap.set('n', '<leader>ce', fzf_lua.diagnostics_document, { desc = 'Open diagnostics' })
-vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Format buffer' })
+vim.keymap.set('n', '<leader>cf', format, { desc = 'Format buffer' })
 vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename symbol' })
 vim.keymap.set('n', '<leader>cs', fzf_lua.lsp_document_symbols, { desc = 'Open symbols' })
 vim.keymap.set('n', '<leader>cx', fzf_lua.lsp_references, { desc = 'Open references' })
