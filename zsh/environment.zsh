c29b8e68ef110555633dd608a2d006a6e9b9200c
Stanislav Gatev <sg@sgatev.com>
2026-09-07 22:37:29 +0200

    Add rustup bin to PATH

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,6 +1,6 @@
 export EDITOR="nvim"
 export MANPAGER="nvim +Man!"
-export PATH="$PATH:$HOME/bin"
+export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
 export PROMPT=$'%F{blue}%~\n%(?.%F{green}❯.%F{red}❯)%f '
 export WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
 
