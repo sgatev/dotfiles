934872264f5fef5d59fc1daf0f7b3bc04058cee1
Stanislav Gatev <sg@sgatev.com>
2026-09-12 07:33:54 +0200

    Use emacs key bindings explicitly

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -1,2 +1,6 @@
+# zsh infers its keymap from $EDITOR, and "nvim" matches *vi*, so the emacs
+# bindings below have to be asked for explicitly.
+bindkey -e
+
 # <Command+Delete> deletes from current position until beginning of line.
 bindkey "^U" backward-kill-line
